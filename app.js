afeaaa78c0430d95b07b2137858a7de034c12c63
jehadjoy55 <joyjehad28@gmail.com>
2024-11-23 18:23:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3000 });
 
-const server = new WebSocket.Server({ port: 3000 });
 let clients = [];
 
-server.on("connection", (ws) => {
-    clients.push(ws);
-    console.log("New client connected.");
+wss.on("connection", (ws) => {
+  clients.push(ws);
+  console.log("A new user connected");
 
-    ws.on("message", (message) => {
-        const data = JSON.parse(message);
-
-        // Broadcast the message to all clients
-        clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify({ message: data.message }));
-            }
-        });
+  // When a message is received, broadcast it to all other clients
+  ws.on("message", (message) => {
+    clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
     });
+  });
 
-    ws.on("close", () => {
-        clients = clients.filter((client) => client !== ws);
-        console.log("Client disconnected.");
-    });
+  // When a client disconnects, remove it from the list of clients
+  ws.on("close", () => {
+    clients = clients.filter((client) => client !== ws);
+    console.log("A user disconnected");
+  });
 });
 
-console.log("WebSocket server is running on ws://localhost:3000");
+console.log("WebSocket server running on ws://localhost:3000");
